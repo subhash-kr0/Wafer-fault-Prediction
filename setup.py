fe2a4bfbdbcbee5e19f5edf90ba4c61e53d605ba
Subhash Kumar <subhashkr855@gmail.com>
2024-12-13 14:23:06 +0530

    Project Structure Completed

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="src",
     version="0.0.1",
     author="Subhash Kumar",
-    author_email="subhashkr855@gmail.com"
+    author_email="subhashkr855@gmail.com",
+    packages=find_packages(),
 )
